b349b8da9258d028069204dc121c0626c29eda00
Nabil ZOUITA <38770876+NabilZouita@users.noreply.github.com>
2019-11-05 17:05:25 +0100

    4th commit
    It workes:
    - Two test cases passes correctly.
    - Function vowels and consonants still uneccessary.

--- minionGame.py
+++ minionGame.py
@@ -4,6 +4,7 @@ Created on Thu Oct 31 15:14:00 2019
 
 @author: nzouita
 """
+import re
 from itertools import chain, combinations
 
 
@@ -23,32 +24,42 @@ def substringSearch(string):
     s = list(string)
     statement = list(map("".join, chain.from_iterable(combinations(s, r) for r in range(len(s)+1))))
     statement.remove(statement[0])
-    print("Current combination is : {}".format(statement))
+    #print("Current combination is : {}".format(statement))
     newList = []
     for i in range(len(statement)):
         if (statement[i] in string):
             newList.append(statement[i])
 
-    print("List after changes: {}".format(newList))
-    print(len(newList))
+    #print("List after changes: {}".format(newList))
+    #print(len(newList))
             
     newList = list(dict.fromkeys(newList))
-    print(newList)
-            
+    #print(newList)
+    
+    indexList = []
+    for i in range(len(newList)):
+        indexList.append([m.start() for m in re.finditer(newList[i],string)])
+        
+    
+    #print(indexList)
 
+#### Find all substring occurences in a string ###            
+#import re
+#[m.start() for m in re.finditer('test', 'test test test test')]
 
-    return newList            
+    return (newList, indexList)            
 
 
 def winnerGame(string):
     kevin = 0
     stuart = 0
-    newList = substringSearch(string)
-    for i in range(len(newList)):
-        if (newList[i][0] in ['A','E','I','O','U']):
-            kevin += 1
+    stringList = substringSearch(string)[0]
+    occList = substringSearch(string)[1]
+    for i in range(len(stringList)):
+        if (stringList[i][0] in ['A','E','I','O','U']):
+            kevin += len(occList[i])
         else:
-            stuart += 1
+            stuart += len(occList[i])
             
     if (kevin > stuart):
         print("{} {}".format("Kevin", kevin))
